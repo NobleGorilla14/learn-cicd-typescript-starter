ca6cf94e1235d2d5af88e706183f78ce7eedbf3a
NobleGorilla14 <K1LLT3K@hotmail.com>
2026-09-22 16:00:55 -0500

    Force failing test to confirm function

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -13,7 +13,7 @@ describe("getAPIKey", () => {
       authorization: "Bearer my-token",
     };
     const result = getAPIKey(headers);
-    expect(result).toBeNull();
+    expect(result).not.toBeNull();
   });
 
   test("should return null if the authorization header is malformed", () => {
